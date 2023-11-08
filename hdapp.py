8e614f15b08541a3b66748d7f50f66ac59623d1d
Joshua Fayele <98998174+JoshuaFayele@users.noreply.github.com>
2023-11-08 23:29:15 +0100

    Update hdapp.py

--- hdapp.py
+++ hdapp.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 st.title("Heart Disease Prediction Application")
-
+st.subheader("This application predicts if a patient has _heart disease_ from their given features")
 
