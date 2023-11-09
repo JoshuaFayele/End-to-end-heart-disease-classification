7f66f9a91afb4f28277d4ff14507a8320a40c66c
Joshua Fayele <98998174+JoshuaFayele@users.noreply.github.com>
2023-11-09 17:54:40 +0100

    update

--- lst.py
+++ lst.py
@@ -3,6 +3,9 @@
 # Core Packages
 import streamlit as st
 
+st.title("My Streamlit Learning File")
+
+
 # Working with and Displaying Text
 st.text("Hello World, this is a text")
 name = "Joshua"
@@ -46,4 +49,53 @@ import pandas as pd
 df = pd.read_csv("D:\Downloads\iris.csv")
 
 # Method 1
-st.dataframe(df)
+#st.dataframe(df)
+
+# Add height
+#st.dataframe(df, 2000, 500)
+
+# Adding a colour style from pandas
+#st.dataframe(df.style.highlight_max(axis=0))
+
+# Method 2: Static Table
+#st.table(df)
+
+# Method 3: Using superfunction 'st.write'
+st.write(df.head())
+
+# Display Json
+st.json({'data':'name'})
+
+# Display Code
+mycode = """
+def sayhello():
+    print("Hello Streamlit Universe")
+"""
+st.code(mycode, language='python')
+
+# Working with Widgets
+# Buttons/Radio?Checkbox/Select/
+
+# Working with Buttons
+last_name = "Fayele"
+first_name = "Joshua"
+if st.button("Submit"):
+    st.write("Last Name: {}".format(last_name.upper()))
+
+if st.button("Submit", key='new1'):
+    st.write("First Name: {}".format(first_name.title()))
+
+# Working with RadioButtons
+status = st.radio("What us your status", ("Active", "Inactive"))
+if status == 'Active':
+    st.success("You are active")
+elif status == "Inactive":
+    st.warning("Inactive")
+    
+# Working with Checkbox
+if st.checkbox("Show/Hide"):
+    st.text("Showing something")
+
+# Working with Beta Expander
+if st.beta_expander
+    st.success("Hello World")
