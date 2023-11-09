f4128c8134f45e50e4d34f43146b7a65a0f6571c
Joshua Fayele <98998174+JoshuaFayele@users.noreply.github.com>
2023-11-09 16:49:38 +0100

    update

--- hdapp.py
+++ hdapp.py
@@ -5,12 +5,10 @@ import numpy as np
 #st.set_page_config(layout="wide")
 st.title("Heart Disease Prediction Application")
 st.header("This application predicts if a patient has _heart disease_ from their given features")
-
-#df = pd.read_csv("C:\Users\USER\Projects\End-to-end-heart-disease-classification\heart-disease.csv")
-#st.table(df)
-
-#title = st.text_input('Movie title', 'Life of Brian')
-#st.write('The current movie title is', title)
-
 name = "Joshua Fayele"
-st.text("This is application was developed by {}".format(name))
+st.markdown("_This application is developed by Joshua Fayele_")
+
+st.subheader("Heart Disease Table")
+df = pd.read_csv("D:\Downloads\heart-disease.csv")
+st.dataframe(df)
+
--- lst.py
+++ lst.py
@@ -3,3 +3,47 @@
 # Core Packages
 import streamlit as st
 
+# Working with and Displaying Text
+st.text("Hello World, this is a text")
+name = "Joshua"
+st.text("My name is {}".format(name))
+
+# Title
+st.title("This is a title")
+
+# Header
+st.header("This is a header")
+
+# Subheader
+st.subheader("This is a subheader")
+
+# Markdown
+st.markdown("This is a markdown")
+
+
+# Displaying Coloured Text/Bootstraps Alert
+st.success("Successful")
+st.warning("Warning")
+st.info("This is an information")
+st.error("This is an error")
+st.exception("This is an exception")
+
+
+# Superfunction
+st.write("This is a normal text") # Text
+st.write("## This is a markdown text") # Markdown
+st.write(1+2) # Math function
+
+st.write(dir(st))
+
+# Help Info
+st.help(range)
+
+# Load EDA Packages
+import pandas as pd
+
+# Display Data
+df = pd.read_csv("D:\Downloads\iris.csv")
+
+# Method 1
+st.dataframe(df)
