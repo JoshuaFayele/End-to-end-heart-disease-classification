a9903afa0949321ad516a88725e7960b3370de3e
Joshua Fayele <98998174+JoshuaFayele@users.noreply.github.com>
2023-11-10 21:08:39 +0100

    update

--- lst.py
+++ lst.py
@@ -3,99 +3,121 @@
 # Core Packages
 import streamlit as st
 
-st.title("My Streamlit Learning File")
+# st.title("My Streamlit Learning File")
 
 
-# Working with and Displaying Text
-st.text("Hello World, this is a text")
-name = "Joshua"
-st.text("My name is {}".format(name))
+# # Working with and Displaying Text
+# st.text("Hello World, this is a text")
+# name = "Joshua"
+# st.text("My name is {}".format(name))
 
-# Title
-st.title("This is a title")
+# # Title
+# st.title("This is a title")
 
-# Header
-st.header("This is a header")
+# # Header
+# st.header("This is a header")
 
-# Subheader
-st.subheader("This is a subheader")
+# # Subheader
+# st.subheader("This is a subheader")
 
-# Markdown
-st.markdown("This is a markdown")
-
-
-# Displaying Coloured Text/Bootstraps Alert
-st.success("Successful")
-st.warning("Warning")
-st.info("This is an information")
-st.error("This is an error")
-st.exception("This is an exception")
+# # Markdown
+# st.markdown("This is a markdown")
 
 
-# Superfunction
-st.write("This is a normal text") # Text
-st.write("## This is a markdown text") # Markdown
-st.write(1+2) # Math function
+# # Displaying Coloured Text/Bootstraps Alert
+# st.success("Successful")
+# st.warning("Warning")
+# st.info("This is an information")
+# st.error("This is an error")
+# st.exception("This is an exception")
 
-st.write(dir(st))
 
-# Help Info
-st.help(range)
+# # Superfunction
+# st.write("This is a normal text") # Text
+# st.write("## This is a markdown text") # Markdown
+# st.write(1+2) # Math function
 
-# Load EDA Packages
-import pandas as pd
+# st.write(dir(st))
 
-# Display Data
-df = pd.read_csv("D:\Downloads\iris.csv")
+# # Help Info
+# st.help(range)
 
-# Method 1
-#st.dataframe(df)
+# # Load EDA Packages
+# import pandas as pd
 
-# Add height
-#st.dataframe(df, 2000, 500)
+# # Display Data
+# df = pd.read_csv("D:\Downloads\iris.csv")
 
-# Adding a colour style from pandas
-#st.dataframe(df.style.highlight_max(axis=0))
+# # Method 1
+# #st.dataframe(df)
 
-# Method 2: Static Table
-#st.table(df)
+# # Add height
+# #st.dataframe(df, 2000, 500)
 
-# Method 3: Using superfunction 'st.write'
-st.write(df.head())
+# # Adding a colour style from pandas
+# #st.dataframe(df.style.highlight_max(axis=0))
 
-# Display Json
-st.json({'data':'name'})
+# # Method 2: Static Table
+# #st.table(df)
 
-# Display Code
-mycode = """
-def sayhello():
-    print("Hello Streamlit Universe")
-"""
-st.code(mycode, language='python')
+# # Method 3: Using superfunction 'st.write'
+# st.write(df.head())
 
-# Working with Widgets
-# Buttons/Radio?Checkbox/Select/
+# # Display Json
+# st.json({'data':'name'})
 
-# Working with Buttons
-last_name = "Fayele"
-first_name = "Joshua"
-if st.button("Submit"):
-    st.write("Last Name: {}".format(last_name.upper()))
+# # Display Code
+# mycode = """
+# def sayhello():
+#     print("Hello Streamlit Universe")
+# """
+# st.code(mycode, language='python')
 
-if st.button("Submit", key='new1'):
-    st.write("First Name: {}".format(first_name.title()))
+# # Working with Widgets
+# # Buttons/Radio?Checkbox/Select/
 
-# Working with RadioButtons
-status = st.radio("What us your status", ("Active", "Inactive"))
-if status == 'Active':
-    st.success("You are active")
-elif status == "Inactive":
-    st.warning("Inactive")
+# # Working with Buttons
+# last_name = "Fayele"
+# first_name = "Joshua"
+# if st.button("Submit"):
+#     st.write("Last Name: {}".format(last_name.upper()))
+
+# if st.button("Submit", key='new1'):
+#     st.write("First Name: {}".format(first_name.title()))
+
+# # Working with RadioButtons
+# status = st.radio("What us your status", ("Active", "Inactive"))
+# if status == 'Active':
+#     st.success("You are active")
+# elif status == "Inactive":
+#     st.warning("Inactive")
     
-# Working with Checkbox
-if st.checkbox("Show/Hide"):
-    st.text("Showing something")
+# # Working with Checkbox
+# if st.checkbox("Show/Hide"):
+#     st.text("Showing something")
 
-# Working with Beta Expander
-if st.beta_expander
-    st.success("Hello World")
+# # Working with Beta Expander
+# if st.expander("Python"):
+#    st.success("Hello Python")
+
+# with st.expander(label="Julia"):
+#     st.text("Hello Julia")
+
+# Select?Multiple select
+my_lang = ["Python", "Julia", "Go", "Rust"]
+
+choice = st.selectbox("Language", my_lang)
+st.write("You selected {}".format(choice))
+
+# Multiple Selection
+spoken_lang = ("English", "French", "Spanish", "Twi")
+my_spoken_lang = st.multiselect("Spoken Language", spoken_lang, default="English")
+
+# Slider
+# Numbers (Int/Float/Dates)
+age = st.slider("Age", 1, 100)
+
+# Any Datatype
+# Select Slider
+colour = st.select_slider("Choose Colour", options=["yellow", "red", "blue", "black", "white"], value=("yellow", "red"))
+
